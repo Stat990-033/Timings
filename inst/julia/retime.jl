d06546988b2adc798c0e178ee4108d6e29c71dff
Douglas Bates <dmbates@gmail.com>
2015-05-26 16:39:04 -0500

    Allow retime.jl to check both with and without gradient.  Update some results.

--- inst/julia/retime.jl
+++ inst/julia/retime.jl
@@ -1,13 +1,17 @@
 using DataFrames,JSON,MixedModels,RCall
 
-function retime(fnm)
+function retime(fnm;repeet::Bool=false)
     js = JSON.parsefile(fnm)
     for k in keys(js)
         println(k)
         dat = DataFrame(k)
         for mm in js[k]
-            form = eval(parse(mm["formula"]))
-            if !haskey(mm,"lmm")
+            ff = mm["formula"]
+            println(ff)
+            form = eval(parse(ff))
+            if (repeet || !haskey(mm,"lmm"))
+                println("lmm")
+                lm = lmm(form,dat)
                 fm = fit(lmm(form,dat))
                 gc()
                 tt = @elapsed(fit(lmm(form,dat)))
@@ -15,6 +19,15 @@ function retime(fnm)
                              :theta=>MixedModels.θ(fm),
                              :hasgrad=>hasgrad(fm),
                              :time=>tt]
+                if (hasgrad(fm))
+                    println("lmmng")
+                    fm = fit(lmm(form,dat),false,false)
+                    gc()
+                    tt = @elapsed(fit(lmm(form,dat),false,false))
+                    mm["lmmng"] = [:deviance=>deviance(fm),
+                                   :theta=>MixedModels.θ(fm),
+                                   :time=>tt]
+                end
             end
         end
     end
