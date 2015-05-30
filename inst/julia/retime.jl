9177d5952b84055a1b3f6e2679da6bdb88f728fc
Douglas Bates <dmbates@gmail.com>
2015-05-30 15:34:49 -0500

    More timings.  Revise the julia code.

--- inst/julia/retime.jl
+++ inst/julia/retime.jl
@@ -3,9 +3,11 @@ using DataFrames,JSON,MixedModels,RCall
 function retime(fnm,ofile)
     js = JSON.parsefile(fnm)
     dsname = js["dsname"]
+    @show dsname
     dat = DataFrame(string("Timings::",dsname))
     js["n"] = size(dat,1)
     js["CPU"] = Sys.cpu_info()[1].model
+    js["CPU_CORES"] = CPU_CORES
     js["OS"] = Sys.OS_NAME
     js["Julia"] = string(VERSION)
     js["WORD_SIZE"] = Sys.WORD_SIZE
@@ -13,23 +15,28 @@ function retime(fnm,ofile)
     js["memory"] = string(@sprintf("%.3f ",Sys.total_memory()/2^30),"GB")
     for m in js["models"]
         form = eval(parse(m["formula"]))
+        @show form
         mod = lmm(form,dat)
         m["nopt"] = length(MixedModels.θ(mod))
         m["mtype"] = typeof(mod.s)
         for f in m["fits"]
-            println(f["function"],": ",f["optimizer"])
+            print(f["function"],": ",f["optimizer"])
             if f["function"] == "lmm"
                 gc()
                 f["time"] = @elapsed mod = fit(lmm(form,dat),false,symbol(f["optimizer"]))
                 f["deviance"] = mod.opt.fmin
+                print(" ",mod.opt.fmin,", ",f["time"])
                 f["feval"] = mod.opt.feval
                 f["geval"] = mod.opt.geval
                 m["p"] = size(mod.X,2)
                 m["q"] = Int[length(b) for b in mod.b]
             end
+            println()
         end
     end
     open(ofile,"w") do io
         write(io,json(js,2))
     end
 end
+
+retime(fnm) = retime(fnm,fnm)
