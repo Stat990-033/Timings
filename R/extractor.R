a2484f45901d69ef9b6829b0622afb4d165f5724
Douglas Bates <dmbates@gmail.com>
2015-06-01 14:14:12 -0500

    Add another subject/item data set.  Create an extractor function.

---
+++ R/extractor.R
@@ -0,0 +1,17 @@
+revisefr <- function(f) {
+    names(f)[names(f)=="deviance"] <- "dev"
+    f$dev <- round(f$dev,2)
+    names(f)[names(f)=="function"] <- "fun"
+    f$optimizer <- ifelse(is.na(f$algorithm),f$optimizer,f$algorithm)
+    f <- subset(f,select=-c(algorithm,geval))
+    f$time <- sapply(f$time,function(v)v[length(v)])
+    f[order(f$dev,f$time),]
+}
+
+#' Extract information on the model fits from a JSON package as data frames
+#'
+#' @param fin name of the input JSON file
+#' @return a list of data frames.  Each frame is ordered by deviance (rounded to 2 decimal places)
+#'    and by elapsed time withing deviance.
+#' @export
+extractor <- function(fin) lapply(fromJSON(fin)$models$fits,revisefr)
