83db4d08a36085427bcf0cd289247b243dceb044
Douglas Bates <dmbates@gmail.com>
2015-09-29 15:02:26 -0500

    Remove LD_* optimizers, retime using partitioned Julia method

--- inst/julia/retime.jl
+++ inst/julia/retime.jl
@@ -4,7 +4,7 @@ function retime(fnm,ofile)
     js = JSON.parsefile(fnm)
     dsname = js["dsname"]
     @show dsname
-    dat = DataFrame(string("Timings::",dsname))
+    dat = rcopy(reval(string("Timings::",dsname)))
     js["n"] = size(dat,1)
     js["CPU"] = Sys.cpu_info()[1].model
     js["CPU_CORES"] = CPU_CORES
@@ -12,26 +12,24 @@ function retime(fnm,ofile)
     js["Julia"] = string(VERSION)
     js["WORD_SIZE"] = Sys.WORD_SIZE
     js["BLAS"] = Base.blas_vendor()
+    blas_set_num_threads(1)
     js["memory"] = string(@sprintf("%.3f ",Sys.total_memory()/2^30),"GB")
     for m in js["models"]
         form = eval(parse(m["formula"]))
         @show form
         mod = lmm(form,dat)
-        nopt = length(MixedModels.θ(mod))
-        mtype = typeof(mod.s)
-        @show(nopt,mtype)
-        m["mtype"] = mtype
+        nopt = length(mod[:θ])
         m["nopt"] = nopt
-        println("-2log(likelihood) time(s) feval geval optimizer") 
+        println("-2log(likelihood) time(s) feval geval optimizer")
         for f in m["fits"]
             if f["func"] == "lmm"
                 gc()
                 f["time"] = @elapsed mod = fit(lmm(form,dat),false,symbol(f["optimizer"]))
-                f["dev"] = deviance(mod)
+                f["dev"] = objective(mod)
                 f["feval"] = mod.opt.feval
                 f["geval"] = mod.opt.geval
-                m["p"] = size(mod.X,2)
-                m["q"] = Int[length(b) for b in mod.b]
+                m["p"] = size(mod.trms[end],2) - 1
+#                m["q"] = Int[length(b) for b in mod.b]
             end
             geval = get(f,"geval",0)
             @printf("%14.4f %10.4f%s%6d %s",f["dev"],f["time"],
